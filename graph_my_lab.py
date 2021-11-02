e71b66c8a9ed4a34e2e845b0e6405b5996739fff
Tox1k <sashasolud@gmail.com>
2021-11-02 18:11:12 +0100

    cleaner code and added support for multiple ifconfig formats

--- graph_my_lab.py
+++ graph_my_lab.py
@@ -6,19 +6,23 @@ import sys
 import ipaddress
 import json
 
-
-#print ('Number of arguments:', len(sys.argv), 'arguments.')
-#print ('Argument List:', str(sys.argv))
+#--TODO-- Add better args parser
 if len(sys.argv) < 2:
     print('usage: {} kathara_lab_DIR/!'.format(sys.argv[0]))
     quit()
+
 DIR = sys.argv[1]
-domain_map = dict()
-hosts = list()
-routers = list()
-domains = list()
 ifaces = dict()
-domain_network_ip = dict()
+domain_map = dict()
+images = {
+  'internet':'assets/internet_1.png',
+  'router': 'assets/router_red.png',
+  'network': 'assets/cloud_1.png',
+  'host': 'assets/host_1.png',
+  'unknown_device': 'assets/host_2.png'
+}
+
+G = nx.Graph()
 
 def parse_lab_conf(line):
     parts = line.split('=')
@@ -26,11 +30,17 @@ def parse_lab_conf(line):
     name = name_if[0]
     iface = 'eth' + name_if[1]
     domain = parts[1]
-    #print('{}[{}] => {}'.format(name,iface,domain))
     return [domain, name, iface]
+  
+def parse_startup_line(line):
+    items = line.split(' ')
+    interface = items[1]
+    if len(items) <= 3:
+      return [interface, ipaddress.IPv4Interface(items[2])]
+    else:
+      return [interface, ipaddress.IPv4Interface(items[2] + '/' + items[4])]
 
-
-G = nx.Graph()
+    
 
 with open(DIR + "/lab.conf", 'r') as file:
     for line in file.read().split('\n'):
@@ -38,18 +48,11 @@ with open(DIR + "/lab.conf", 'r') as file:
             continue
         
         domain, name, iface = parse_lab_conf(line)
-        if domain not in domain_map:
-            domain_map[domain] = list()
-        domain_map[domain].append(name)
+        domain_map[domain] = 'UNDEFINED'
 
-        if name not in hosts:
-            if(name.startswith('r')):
-                routers.append(name)
-            if(name.startswith('pc') or name.startswith('h')):
-                hosts.append(name)
-        
-        if domain not in domains:
-            domains.append(domain)
+        if not (name.startswith('pc') or name.startswith('r') or name.startswith('h')):
+          print("{} found: Please use either pcx or hx or rx as your host names!".format(name))
+          quit()
 
         if name not in ifaces:
           ifaces[name] = dict()
@@ -57,56 +60,46 @@ with open(DIR + "/lab.conf", 'r') as file:
         ifaces[name][iface] = dict()
         ifaces[name][iface]['domain'] = domain
 
-#print(ifaces)
-#print(domain_map)
-#print('hosts: ', hosts)
-#print('domains: ',domains)
-
-for host in hosts + routers:
-  with open(DIR + "/" + host + ".startup", 'r') as f:
+for asset in ifaces:
+  with open(DIR + "/" + asset + ".startup", 'r') as f:
     for line in f.read().split('\n'):
       if not line.startswith('ifconfig'):
         continue
-      _, iface, ip, _, netmask = line.split(' ')
-      #print(iface,ip,netmask)
-      interface = ipaddress.IPv4Interface(ip + '/' + netmask)
-      ifaces[host][iface]['ip'] = ip
-      ifaces[host][iface]['netmask'] = netmask
-      ifaces[host][iface]['beautiful_ip'] = str(interface)
-      #print(interface.network)
-      network_ip = str(interface.network)
-      domain = ifaces[host][iface]['domain']
-      if domain not in domain_network_ip:
-        domain_network_ip[domain] = network_ip
-      else:
-        if domain_network_ip[domain] != network_ip:
-          print('network ip override! check al the connected interfaces!')
+      iface, ip = parse_startup_line(line)
+      ifaces[asset][iface]['ip'] = str(ip.ip)
+      ifaces[asset][iface]['netmask'] = str(ip.netmask)
+      network_ip = str(ip.network)
+      domain = ifaces[asset][iface]['domain']
+      #--TODO--create a domain.conf file for better error correction 
+      if domain_map[domain] == 'UNDEFINED':
+        domain_map[domain] = network_ip
+      elif domain_map[domain] != network_ip:
+          print('network ip override! check interfaces connected to [{}] domain!'.format(domain))
           quit()
-#print(domain_network_ip)
 
-#options = {'color' : 'blue', 'shape':'square', 'group':['a', 'b']}
-#options = {'color' : 'blue', 'shape':'square'}
-options = {'shape' : 'image', 'image' : 'assets/host_1.png', 'size' : '20'}
-colored_hosts = map(lambda node: (node, options), hosts)
-colored_hosts = list(colored_hosts)
-G.add_nodes_from(colored_hosts)
+for asset in ifaces:
+  G.add_node(asset)
+  G.nodes[asset]['shape'] = 'image'
+  if asset.startswith('r'):
+    G.nodes[asset]['image'] = images['router']
+    G.nodes[asset]['size'] = 25
+  elif asset.startswith('h') or asset.startswith('pc'):
+    G.nodes[asset]['image'] = images['host']
+    G.nodes[asset]['size'] = 20
+  else:
+    G.nodes[asset]['image'] = images['unknown_device']
+    G.nodes[asset]['size'] = 20
 
-#options = {'color' : 'red'}
-#options = {'color' : 'red', 'shape' : 'image', 'image' : 'assets/router_red.png', 'size' : '50'}
-options = {'shape' : 'image', 'image' : 'assets/router_red.png', 'size' : '25'}
-colored_routers = map(lambda node: (node, options), routers)
-colored_routers = list(colored_routers)
-G.add_nodes_from(colored_routers)
 
-#options = {'color' : 'green', 'shape':'diamond'}
-options = {'shape' : 'image', 'image' : 'assets/cloud_1.png', 'size' : '20'}
-colored_domains = map(lambda node: (node, options), domains)
-colored_domains = list(colored_domains)
-G.add_nodes_from(colored_domains)
-for domain, net_ip in domain_network_ip.items():
+for domain, net_ip in domain_map.items():
+  G.add_node(domain)
   G.nodes[domain]['label'] = domain + '\n' + net_ip
+  G.nodes[domain]['image'] = images['network']
+  G.nodes[domain]['shape'] = 'image'
+  G.nodes[domain]['size'] = 20
+
 if 'INTERNET' in G.nodes:
-  G.nodes['INTERNET']['image'] = 'assets/internet_1.png'
+  G.nodes['INTERNET']['image'] = images['internet']
   G.nodes['INTERNET']['size'] = '30'
   G.nodes['INTERNET']['label'] = 'INTERNET'
 
@@ -115,36 +108,23 @@ for host, host_ifaces in ifaces.items():
   for interface, params in host_ifaces.items():
     domain = params['domain']
     ip = params['ip']
-    #print(host,interface,ip,' -> ', domain)
     G.add_edge(host, domain, label = interface + '\n' + ip)
 
-print(json.dumps(ifaces))
+#print(json.dumps(ifaces))
+#print(json.dumps(domain_map))
 
 options = """{
-  "nodes": {
-    "font": {
-      "size": 15
-    },
-    "size": 70
-  },
   "edges": {
     "color": {
       "inherit": false
     },
-    "smooth": false
-  },
-  "layout": {
-    "hierarchical": {
-      "enabled": false
-    }
+    "smooth": true
   },
   "interaction": {
     "navigationButtons": true
   },
   "physics": {
-    "enabled": false,
-    "minVelocity": 0.75,
-    "solver": "hierarchicalRepulsion"
+    "enabled": false
   }
 }"""
 
@@ -153,6 +133,4 @@ net.from_nx(G)
 #net.show_buttons()
 net.set_options(options)
 net.show("graph.html")
-
-
 
