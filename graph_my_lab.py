cb6dd2484084d2bba98a8862ed17448eaeca971d
Tox1k <sashasolud@gmail.com>
2021-12-11 00:16:27 +0100

    Better warnings and added ip collision check

--- graph_my_lab.py
+++ graph_my_lab.py
@@ -8,11 +8,11 @@ import json
 
 #--TODO-- Add better args parser
 if len(sys.argv) < 2:
-    print('usage: {} kathara_lab_DIR/!'.format(sys.argv[0]))
+    print('usage: {} kathara_lab_DIR/'.format(sys.argv[0]))
     quit()
 
 DIR = sys.argv[1]
-ifaces = dict()
+nodes = dict()
 domain_map = dict()
 images = {
   'internet':'assets/internet_1.png',
@@ -23,6 +23,15 @@ images = {
 }
 
 G = nx.Graph()
+
+def get_devices_by_domain(domain):
+  ret = []
+  for device,ifaces in nodes.items():
+            for iface, data in ifaces.items():
+                if len(data)>1:
+                  if data['domain'] == domain:
+                    ret.append({'name': device,'iface': iface, 'ip': data['ip']})
+  return ret
 
 def parse_lab_conf(line):
     parts = line.split('=')
@@ -51,44 +60,70 @@ with open(DIR + "/lab.conf", 'r') as file:
         domain_map[domain] = 'UNDEFINED'
 
         if not (name.startswith('pc') or name.startswith('r') or name.startswith('h')):
-          print("{} found: Please use either pcx or hx or rx as your host names!".format(name))
-          quit()
+          print("[WARNING] '{}' found: Please use either pcx or hx or rx as your host names!".format(name))
+          #quit()
 
-        if name not in ifaces:
-          ifaces[name] = dict()
+        if name not in nodes:
+          nodes[name] = dict()
           
-        ifaces[name][iface] = dict()
-        ifaces[name][iface]['domain'] = domain
+        nodes[name][iface] = dict()
+        nodes[name][iface]['domain'] = domain
+error = 0
+for device in nodes:
+  with open(DIR + "/" + device + ".startup", 'r') as f:
 
-for asset in ifaces:
-  with open(DIR + "/" + asset + ".startup", 'r') as f:
     for line in f.read().split('\n'):
       if not line.startswith('ifconfig'):
         continue
       iface, ip = parse_startup_line(line)
-      ifaces[asset][iface]['ip'] = str(ip.ip)
-      ifaces[asset][iface]['netmask'] = str(ip.netmask)
+      nodes[device][iface]['ip'] = str(ip.ip)
+      nodes[device][iface]['netmask'] = str(ip.netmask)
       network_ip = str(ip.network)
-      domain = ifaces[asset][iface]['domain']
+      domain = nodes[device][iface]['domain']
       #--TODO--create a domain.conf file for better error correction 
       if domain_map[domain] == 'UNDEFINED':
         domain_map[domain] = network_ip
       elif domain_map[domain] != network_ip:
-          print('network ip override! check interfaces connected to [{}] domain!'.format(domain))
-          quit()
+          error = 1
+          print('\n[ERROR] \'{}\' network ip override! check interfaces connected to domain!'.format(domain))
+          print('\tconnected devices are:')
+          for device,ifaces in nodes.items():
+            for iface, data in ifaces.items():
+                if len(data)>1:
+                  if data['domain'] == domain:
+                    print('\t{}[{}] has {}'.format(device, iface, data['ip']))
+          print('previously defined network ip: {}\nnew network ip: {}'.format(domain_map[domain], network_ip))
 
-for asset in ifaces:
-  G.add_node(asset)
-  G.nodes[asset]['shape'] = 'image'
-  if asset.startswith('r'):
-    G.nodes[asset]['image'] = images['router']
-    G.nodes[asset]['size'] = 25
-  elif asset.startswith('h') or asset.startswith('pc'):
-    G.nodes[asset]['image'] = images['host']
-    G.nodes[asset]['size'] = 20
+#check for ip collision on same domain
+for domain, net_id in domain_map.items():
+  _devices = get_devices_by_domain(domain)
+  devices = _devices
+  for device in _devices:
+    devices.remove(device)
+    for other in devices:
+      if other['ip'] == device['ip']:
+        error = 1
+        print('\n[ERROR] Duplicate found:')
+        print('{}:{} and {}:{} both share ip {} on domain {}'.format(device['name'], device['iface'], other['name'], other['iface'], device['ip'], domain))
+    
+    devices.append(device)
+
+if error == 1:
+  quit()
+
+
+for device in nodes:
+  G.add_node(device)
+  G.nodes[device]['shape'] = 'image'
+  if device.startswith('r'):
+    G.nodes[device]['image'] = images['router']
+    G.nodes[device]['size'] = 25
+  elif device.startswith('h') or device.startswith('pc'):
+    G.nodes[device]['image'] = images['host']
+    G.nodes[device]['size'] = 20
   else:
-    G.nodes[asset]['image'] = images['unknown_device']
-    G.nodes[asset]['size'] = 20
+    G.nodes[device]['image'] = images['unknown_device']
+    G.nodes[device]['size'] = 20
 
 
 for domain, net_ip in domain_map.items():
@@ -104,13 +139,13 @@ if 'INTERNET' in G.nodes:
   G.nodes['INTERNET']['label'] = 'INTERNET'
 
 
-for host, host_ifaces in ifaces.items():
+for host, host_ifaces in nodes.items():
   for interface, params in host_ifaces.items():
     domain = params['domain']
     ip = params['ip']
     G.add_edge(host, domain, label = interface + '\n' + ip)
 
-#print(json.dumps(ifaces))
+#print(json.dumps(nodes))
 #print(json.dumps(domain_map))
 
 options = """{
@@ -133,4 +168,4 @@ net.from_nx(G)
 #net.show_buttons()
 net.set_options(options)
 net.show("graph.html")
-
+print('[*] Done, output file generated as graph.html!')
